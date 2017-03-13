07fcbfd693b18ba6c743aee8b11acaf40fc84288
yuanyue <87439247@qq.com>
2017-03-13 16:49:38 +0800

    ngrinder 3.4.1 add docker sample

--- ngrinder/start.sh
+++ ngrinder/start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 ${AGENT_ROOT}/run_agent_bg.sh && /opt/entry.sh
+#run sample is ==========
+#docker run -d --name=ngrinder --net=host -e SERVER_PORT=8585 --privileged=true docker-registry.hexun.com/hexunzq/ngrinder:1.0.0
