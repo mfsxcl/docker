e2e0b387b6d3ded8b72d2e8f118d68986a0585c2
yuanyue <87439247@qq.com>
2017-04-18 00:01:10 +0800

    perfect

--- tengine/convertGBK2UTF8.sh
+++ tengine/convertGBK2UTF8.sh
@@ -4,12 +4,14 @@ target="/opt/inc_target"
 cd ${source} && find . -type d -exec mkdir -p ${target}/{} \;
 cd ${source} && find . -type f -exec iconv -f GBK -t UTF-8 {} -o ${target}/{} \;
 
+log_file=/opt/inotify.log
 #watch file change
 inotifywait -mrq --format '%w%f' -e modify,delete,create,attrib ${source} |  while read file
 do
       target_file=${file/inc_source/inc_target}
-      echo "${file} was changed, saved utf8 to ${target_file}" >> /opt/inotify.log 2>&1
-      dir_name=`dirname ${file}`
+      echo "${file} was changed, saved utf8 to ${target_file}" >> ${log_file}
+      dir_name=`dirname ${target_file}`
       mkdir -p ${dir_name}
+      echo "dir ${dir_name} created" >> ${log_file}
       iconv -f GBK -t UTF-8 ${file} -o ${target_file}
 done
