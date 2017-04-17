3a7e34d9c72a48a4b9c640d1971a8de26213dedb
yuanyue <87439247@qq.com>
2017-04-17 23:47:30 +0800

    监视文件变更,实时转换成 utf8编码碎片

--- tengine/convertGBK2UTF8.sh
+++ tengine/convertGBK2UTF8.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 source="/opt/inc_source"
 target="/opt/inc_target"
-if [ "`ls -A $source`" = "" ]; then
-    echo "$source is indeed empty"
-else
-    cd ${source} && find . -type d -exec mkdir -p ${target}/{} \;
-    cd ${source} && find . -type f -exec iconv -f GBK -t UTF-8 {} -o ${target}/{} \;
-fi
+cd ${source} && find . -type d -exec mkdir -p ${target}/{} \;
+cd ${source} && find . -type f -exec iconv -f GBK -t UTF-8 {} -o ${target}/{} \;
+
+#watch file change
+inotifywait -mrq --format '%w%f' -e modify,delete,create,attrib ${source} |  while read file
+do
+      target_file=${file/inc_source/inc_target}
+      echo "${file} was changed, saved utf8 to ${target_file}" >> /opt/inotify.log 2>&1
+      dir_name=`dirname ${file}`
+      mkdir -p ${dir_name}
+      iconv -f GBK -t UTF-8 ${file} -o ${target_file}
+done
--- tengine/entry.sh
+++ tengine/entry.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#every five minutes convert inc file from charset gbk to utf8
-echo "*/5 * * * * /opt/convertGBK2UTF8.sh > /dev/null 2>&1" >> /var/spool/cron/root
+nohup /opt/convertGBK2UTF8.sh &
 nginx -g 'daemon off;'
